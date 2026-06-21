6205610fbafe29af25a613fe1a377cab4915b711
MFaisalDeveloPer <mdfaisal8310@gmail.com>
2026-06-21 17:16:25 +0530

    Configure production remotes

--- root-config/vite.config.js
+++ root-config/vite.config.js
@@ -5,6 +5,7 @@ import { federation } from "@module-federation/vite";
 export default defineConfig({
   plugins: [
     react(),
+
     federation({
       name: "shell",
 
@@ -12,37 +13,48 @@ export default defineConfig({
         navbar: {
           type: "module",
           name: "navbar",
-          entry: "http://localhost:5174/remoteEntry.js",
+          entry:
+            "https://microfrontend-portfolio-3jzh-e5jae7z7e-mdfaisal.vercel.app/remoteEntry.js",
         },
 
         hero: {
           type: "module",
           name: "hero",
-          entry: "http://localhost:5175/remoteEntry.js",
+          entry:
+            "https://microfrontend-portfolio-by2i.vercel.app/remoteEntry.js",
         },
+
         projects: {
-    type: "module",
-    name: "projects",
-    entry: "http://localhost:5177/remoteEntry.js",
-  },
-    skills: {
-    type: "module",
-    name: "skills",
-    entry: "http://localhost:5179/remoteEntry.js",
-  },
-  experience: {
-  type: "module",
-  name: "experience",
-  entry: "http://localhost:5178/remoteEntry.js",
-},
+          type: "module",
+          name: "projects",
+          entry:
+            "https://microfrontend-portfolio-d2p5.vercel.app/remoteEntry.js",
+        },
+
+        skills: {
+          type: "module",
+          name: "skills",
+          entry:
+            "https://microfrontend-portfolio-oz6a.vercel.app/remoteEntry.js",
+        },
+
+        experience: {
+          type: "module",
+          name: "experience",
+          entry:
+            "https://microfrontend-portfolio-ql3h.vercel.app/remoteEntry.js",
+        },
       },
 
       shared: {
         react: {
           singleton: true,
+          requiredVersion: false,
         },
+
         "react-dom": {
           singleton: true,
+          requiredVersion: false,
         },
       },
 
